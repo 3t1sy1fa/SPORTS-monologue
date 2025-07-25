8ba450fa2cda19c57cdd9f1e4dccf40ac01d9339
Soyul lee <yulyullucy5919@gmail.com>
2025-07-25 02:39:41 +0000

    글 레이아웃

--- .eleventy.js
+++ .eleventy.js
@@ -34,4 +34,20 @@ module.exports = function (eleventyConfig) {
     },
     markdownTemplateEngine: "njk"
   };
+  module.exports = function (eleventyConfig) {
+  // ... 기존 설정들 생략 ...
+
+  // 📌 기본 레이아웃을 post.njk로 설정
+  eleventyConfig.addGlobalData("layout", "post.njk");
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "public"
+    },
+    markdownTemplateEngine: "njk"
+  };
+};
 };
