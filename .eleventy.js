51e76ee6b5f87a3449e7e8159e579b6411eb1007
Soyul lee <yulyullucy5919@gmail.com>
2025-07-25 02:05:30 +0000

    세부사항을 최대한 채웠엉ㅅ

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,23 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("style");
-  eleventyConfig.addPassthroughCopy("scripts");
-  eleventyConfig.addPassthroughCopy("images");
-  eleventyConfig.addPassthroughCopy("fonts");
-  eleventyConfig.addPassthroughCopy("admin");
+module.exports = function (eleventyConfig) {
+  // ⬛ 정적 파일 복사
+  eleventyConfig.addPassthroughCopy("src/style");
+  eleventyConfig.addPassthroughCopy("src/scripts");
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/fonts");
+  eleventyConfig.addPassthroughCopy("src/admin");
+
+  // ⬛ posts 컬렉션 등록
+  eleventyConfig.addCollection("posts", function (collection) {
+    return collection.getFilteredByGlob("src/posts/*.md");
+  });
 
   return {
     dir: {
       input: "src",
-      output: "public"
+      includes: "_includes",
+      data: "_data",
+      output: "public",
     },
-    passthroughFileCopy: true
+    markdownTemplateEngine: "njk",
   };
 };
