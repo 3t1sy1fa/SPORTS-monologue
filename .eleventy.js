4b52fe38f13bfe7e1878428774b214229634375c
Soyul lee <yulyullucy5919@gmail.com>
2025-07-27 06:35:55 +0000

    166

--- .eleventy.js
+++ .eleventy.js
@@ -36,9 +36,9 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "src",
-      includes: "includes", // 이게 틀리면 layout 경로 못 찾음
-      data: "data",
-      output: "public"
+      includes: "_includes", // 이게 틀리면 layout 경로 못 찾음
+      data: "_data",
+      output: "_public"
     }
   };
 };
