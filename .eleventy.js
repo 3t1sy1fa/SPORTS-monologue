738e7be1e0bce3191d4d92816af598a8f5a63350
Soyul lee <yulyullucy5919@gmail.com>
2025-07-27 04:54:30 +0000

    155

--- .eleventy.js
+++ .eleventy.js
@@ -38,7 +38,7 @@ module.exports = function (eleventyConfig) {
       input: "src",           // 콘텐츠 소스 폴더
       includes: "_includes",  // 공통 템플릿 폴더
       data: "_data",          // 글로벌 데이터 폴더
-      output: "public"        // 최종 HTML이 출력될 폴더
+      output: "_site"        // 최종 HTML이 출력될 폴더
     },
     markdownTemplateEngine: "njk"
   };
