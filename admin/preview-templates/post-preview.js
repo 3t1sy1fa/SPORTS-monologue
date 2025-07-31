40036964290dbc84937076231a4f095dc387f777
Soyul lee <yulyullucy5919@gmail.com>
2025-07-31 13:09:36 +0000

    10

--- admin/preview-templates/post-preview.js
+++ admin/preview-templates/post-preview.js
@@ -1,5 +1,5 @@
 import CMS from "netlify-cms-app";
-import "../style/main.css"; // CSS를 미리보기에도 적용
+import "../../style/main.css"; // 사이트 스타일 적용
 
 const PostPreview = ({ entry }) => {
   const title = entry.getIn(["data", "title"]);
@@ -10,9 +10,39 @@ const PostPreview = ({ entry }) => {
     <main class="post-page">
       <h1 class="post-title">${title}</h1>
       <p class="post-category">[${category}]</p>
-      <article class="post-body">${body}</article>
+      <div class="post-body">${body}</div>
+    </main>
+  `;
+};
+
+const LogPreview = ({ entry }) => {
+  const title = entry.getIn(["data", "title"]);
+  const category = entry.getIn(["data", "category"]);
+  const body = entry.getIn(["data", "body"]);
+
+  return `
+    <main class="log-container">
+      <h2>${title}</h2>
+      <p class="log-post-category">[${category}]</p>
+      <div class="log-post-desc">${body}</div>
+    </main>
+  `;
+};
+
+const TeamAnalysisPreview = ({ entry }) => {
+  const title = entry.getIn(["data", "title"]);
+  const category = entry.getIn(["data", "category"]);
+  const body = entry.getIn(["data", "body"]);
+
+  return `
+    <main class="team-analysis">
+      <h1>${title}</h1>
+      <p>팀: ${category}</p>
+      <div class="team-body">${body}</div>
     </main>
   `;
 };
 
 CMS.registerPreviewTemplate("posts", PostPreview);
+CMS.registerPreviewTemplate("log", LogPreview);
+CMS.registerPreviewTemplate("teamAnalysis", TeamAnalysisPreview);
