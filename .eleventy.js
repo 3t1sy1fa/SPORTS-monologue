e49abd0db9af460ad2a363814218afebb3a61d4c
Soyul lee <yulyullucy5919@gmail.com>
2025-08-01 02:32:08 +0000

    14

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("static/favicon.png");
   // ✅ 정적 리소스 그대로 복사
   ["src/style", "src/scripts", "src/images", "src/fonts", "admin"]
     .forEach(path => eleventyConfig.addPassthroughCopy(path));
