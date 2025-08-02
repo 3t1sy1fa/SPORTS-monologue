a54f2125c104b488210ea643e9bec0b579defeec
Soyul lee <yulyullucy5919@gmail.com>
2025-08-02 10:08:31 +0000

    109

--- src/_data/voteSummary.js
+++ src/_data/voteSummary.js
@@ -2,31 +2,22 @@
 const votes = require("./votes.json");
 
 module.exports = () => {
-  const teamVotes = {};
-  const playerVotes = {};
+  const players = {};
 
-  votes.forEach((vote) => {
-    if (vote.targetType === "team") {
-      teamVotes[vote.teamSlug] = (teamVotes[vote.teamSlug] || 0) + 1;
-    }
+  votes.forEach(vote => {
     if (vote.targetType === "player") {
-      playerVotes[vote.playerSlug] = (playerVotes[vote.playerSlug] || 0) + 1;
+      const key = vote.playerSlug;
+      if (!players[key]) {
+        players[key] = {
+          playerSlug: vote.playerSlug,
+          playerName: vote.playerName || "이름 없음",
+          playerTeamSlug: vote.playerTeamSlug || "",
+          playerTotalVotes: 0
+        };
+      }
+      players[key].playerTotalVotes++;
     }
   });
 
-  // 객체 → 배열 변환
-  const teamArray = Object.entries(teamVotes).map(([teamSlug, totalVotes]) => ({
-    teamSlug,
-    teamTotalVotes: totalVotes,
-  }));
-
-  const playerArray = Object.entries(playerVotes).map(([playerSlug, totalVotes]) => ({
-    playerSlug,
-    playerTotalVotes: totalVotes,
-  }));
-
-  return {
-    teams: teamArray,
-    players: playerArray,
-  };
+  return Object.values(players); // ✅ 배열로 반환
 };
