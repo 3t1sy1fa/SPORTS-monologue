fada0be717e1ca8aa3ce1af1fbf4bc9f715da9b1
Soyul lee <yulyullucy5919@gmail.com>
2025-08-02 11:50:23 +0000

    134

--- src/_data/voteSummary.js
+++ src/_data/voteSummary.js
@@ -1,17 +1,8 @@
 const votes = require("./votes.json");
 
 module.exports = () => {
-  const teams = {};
-  const players = {};
-
-  votes.forEach((vote) => {
-    if (vote.targetType === "team") {
-      teams[vote.teamSlug] = (teams[vote.teamSlug] || 0) + 1;
-    }
-    if (vote.targetType === "player") {
-      players[vote.playerSlug] = (players[vote.playerSlug] || 0) + 1;
-    }
-  });
-
-  return { teams, players };
+  return {
+    teams: votes.teams || {},
+    players: votes.players || {},
+  };
 };
